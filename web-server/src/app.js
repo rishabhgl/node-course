c1951cdf56ffad23e6e87b6d8b6fc826e21a096c
rishabhgl <rgoyal2_be21@thapar.edu>
2023-03-02 21:50:11 +0530

    added dynamic templates to pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,33 +1,39 @@
 const path = require('path');
+
 const express = require('express');
+const hbs = require('hbs');
 
 const assets = path.join(__dirname, '../public');
+const viewTemplates = path.join(__dirname, '../templates/views');
+const partialTemplates = path.join(__dirname, '../templates/partials');
 const app = express();
 
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
+app.set('views', viewTemplates);
+hbs.registerPartials(partialTemplates);
 app.use(express.static(assets, { extensions: ['html'] }));
 
 app.get('/', (req, res) => {
-    res.render('index',{
+    res.render('index', {
         title: 'Home Page',
         message: 'Welcome to my first express.js based web application!',
-        author: 'Rishabh Goyal'
+        name: 'Rishabh Goyal'
     })
 })
 
 app.get('/about', (req, res) => {
-    res.render('about',{
+    res.render('about', {
         title: 'About Page',
         message: 'Welcome! This is where you can learn more about us.',
-        author: 'Rishabh Goyal'
+        name: 'Rishabh Goyal'
     })
 })
 
 app.get('/help', (req, res) => {
-    res.render('help',{
+    res.render('help', {
         title: 'Help Page',
         message: 'Looking for some help? You have come to the right place.',
-        author: 'Rishabh Goyal'
+        name: 'Rishabh Goyal'
     })
 })
 
@@ -36,6 +42,21 @@ app.get('/weather', (req, res) => {
         forecast: 'The weather has been obnoxious lately.',
         location: 'Patiala'
     });
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        title: '404 Page',
+        message: 'Help article not found!',
+        name: 'Rishabh Goyal'
+    })
+})
+app.get('*', (req, res) => {
+    res.render('error', {
+        title: '404 Page',
+        message: 'Page not found!',
+        name: 'Rishabh Goyal'
+    })
 })
 
 app.listen(3000, () => {
