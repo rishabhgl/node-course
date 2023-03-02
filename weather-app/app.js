217f27dbfff3bbb7d71217490de90ea39158d5d4
rishabhgl <rgoyal2_be21@thapar.edu>
2023-03-02 09:03:42 +0530

    refactored code

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,21 +1,24 @@
-const request = require('postman-request');
+const geoCode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const urlWeather = 'http://api.weatherstack.com/current?access_key=cf5655c384dea1036d774b4aa0c0ea77&query=39.9526,75.1652&units=f';
+const location = process.argv[2]; 
 
-request({ url: urlWeather, json: true }, (error, response) => {
-    // console.log(JSON.parse(response.body).current);
-    const data = response.body.current;
-    const weatherDesc = `The day seems ${data.weather_descriptions[0]}`;
-    const actualWeather = `. It is currently ${data.temperature} degrees out.`;
-    const apparentWeather = ` But it feels like ${data.feelslike} degrees out.`;
-    console.log(weatherDesc + actualWeather + apparentWeather);
-})
+if (!location)
+{
+    return;
+}
 
-const urlGeo = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmlzaGFiaGdsIiwiYSI6ImNsZXA3MzJ6MzAxYzgzeHFnemJ0emQ4dHkifQ.i4gT4gaTI1O_t9bMQS44hw";
-
-request({url: urlGeo, json: true}, (error, response) => {
-    const geoData = response.body.features[0];
-    const lat = geoData.center[1];
-    const lon = geoData.center[0];
-    console.log('The latitude, longitude coordinates of the given target are: ' + lat + ', ' + lon);
+geoCode(location, (error, data) => {
+    if (error) {
+        console.log(error);
+    } else {
+        forecast( data, (error, weatherdata) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data.name);
+                console.log(weatherdata);
+            }
+        })
+    }
 })
